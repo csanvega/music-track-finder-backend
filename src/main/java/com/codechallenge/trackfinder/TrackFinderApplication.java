86c622ad84f38e0c513975d476d0ad439cb4642d
Carlos Santander <csantanderv@gmail.com>
2025-05-11 16:13:33 -0400

    chore: add initial project setup

---
+++ src/main/java/com/codechallenge/trackfinder/TrackFinderApplication.java
@@ -0,0 +1,13 @@
+package com.codechallenge.trackfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codechallenge/trackfinder/TrackfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codechallenge.trackfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
