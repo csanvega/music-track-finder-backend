7d1d94ace7cce75b83298794db9d799b4202ab97
Carlos Santander <csantanderv@gmail.com>
2025-05-12 01:14:03 -0400

    feat(create-track): add db integration and logic for saving the track data

--- src/main/java/com/codechallenge/trackfinder/service/impl/TrackFinderServiceImpl.java
+++ src/main/java/com/codechallenge/trackfinder/service/impl/TrackFinderServiceImpl.java
@@ -1,17 +1,22 @@
 package com.codechallenge.trackfinder.service.impl;
 
 import com.codechallenge.trackfinder.dto.*;
+import com.codechallenge.trackfinder.entity.Track;
+import com.codechallenge.trackfinder.repository.TrackRepository;
 import com.codechallenge.trackfinder.service.SpotifyApiClientService;
 import com.codechallenge.trackfinder.service.TrackFinderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class TrackFinderServiceImpl implements TrackFinderService {
 
     private final SpotifyApiClientService spotifyApiClientService;
+    private final TrackRepository trackRepository;
 
+    @Transactional
     public TrackDetailsResponse createTrack(String isrc) {
         try {
             SpotifySearchTrackResponse responseSearch = spotifyApiClientService.searchTrack(isrc);
@@ -32,6 +37,23 @@ public class TrackFinderServiceImpl implements TrackFinderService {
             if(responseGetAlbum == null) {
                 throw new Exception("Album was not found");
             }
+
+            SpotifyAlbumImage coverImage = responseGetAlbum.images().stream()
+                    .filter(img -> img.width() == 300)
+                    .findFirst()
+                    .orElseThrow(() -> new Exception("Image of 300x300 was not found"));;
+
+            Track track = new Track();
+            track.setId(trackItem.id());
+            track.setIsrc(isrc);
+            track.setName(trackItem.name());
+            track.setArtistName(artist.name());
+            track.setAlbumName(responseGetAlbum.name());
+            track.setPlaybackSeconds(trackItem.duration_ms());
+            track.setIsExplicit(trackItem.explicit());
+            track.setCoverUrl(coverImage.url());
+
+            trackRepository.save(track);
 
             return TrackDetailsResponse.builder()
                     .name(trackItem.name())
