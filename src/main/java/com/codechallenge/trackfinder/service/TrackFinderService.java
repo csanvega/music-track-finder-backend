078e1ae3165ed6135ac8dd1b3ee94ac46e55134f
Carlos Santander <csantanderv@gmail.com>
2025-05-12 23:30:29 -0400

    feat(add-save-cover): add new endpoint to get the image cover from DB

--- src/main/java/com/codechallenge/trackfinder/service/TrackFinderService.java
+++ src/main/java/com/codechallenge/trackfinder/service/TrackFinderService.java
@@ -1,10 +1,10 @@
 package com.codechallenge.trackfinder.service;
 
-import com.codechallenge.trackfinder.dto.TrackCoverResponse;
+import com.codechallenge.trackfinder.dto.ImageCover;
 import com.codechallenge.trackfinder.dto.TrackDetailsResponse;
 
 public interface TrackFinderService {
     TrackDetailsResponse createTrack(String isrc);
     TrackDetailsResponse getTrackMetadata(String isrc);
-    TrackCoverResponse getCover(String isrc);
+    ImageCover getCover(String isrc);
 }
